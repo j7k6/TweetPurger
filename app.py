1f9362b09b6b9c41113556ac05530c4933522abb
j7k6 <mail@j7k6.org>
2022-08-18 12:19:05 +0200

    added oauth2 + bookmarks feature

--- app.py
+++ app.py
@@ -1,50 +1,45 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, redirect
 import configparser
 import datetime
+import os 
 import threading
 import time
 import tweepy
 import webbrowser
 
 
-def auth_handler(auth_data=None):
-    global access_token, access_token_secret
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-    if auth_data is not None:
-        auth.request_token = { "oauth_token": auth_data["oauth_token"], "oauth_token_secret": auth_data["oauth_verifier"] }
 
-        try:
-            auth.get_access_token(auth_data["oauth_verifier"])
+def auth_handler(access_token):
+    config["auth"]["access_token"] = access_token
 
-            access_token = config["auth"]["access_token"] = auth.access_token
-            access_token_secret = config["auth"]["access_token_secret"] = auth.access_token_secret
+    with open("config.ini", "w") as f:
+        config.write(f)
 
-            with open("config.ini", "w") as f:
-                config.write(f)
+    client = tweepy.Client(access_token)
 
-            print("Login successful!")
-        except tweepy.TweepError as e:
-            print(e)
-            exit()
-
-    client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, wait_on_rate_limit=True)
+    print("Login successful!")
     
     purge_tweets(client)
 
 
 def purge_tweets(client):
-    user = client.get_me().data
+    user = client.get_me(user_auth=False).data
 
     print(f"Purging Tweets for @{user} ({user.id})")
 
     try:
-        bearer_client = tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
+        bookmarks = [bookmark["id"] for bookmark in tweepy.Paginator(client.get_bookmarks, max_results=100).flatten()]
+
         end_time = datetime.datetime.now() - datetime.timedelta(days=delete_days_threshold)
         deleted_tweets = 0
 
-        for tweet in tweepy.Paginator(bearer_client.get_users_tweets, user.id, tweet_fields=["public_metrics"], end_time=end_time, max_results=100).flatten():
-            if tweet.data["public_metrics"]["like_count"] < delete_like_threshold: 
-                client.delete_tweet(tweet.data["id"])
+        for tweet in tweepy.Paginator(client.get_users_tweets, user.id, tweet_fields=["public_metrics"], end_time=end_time, max_results=100).flatten():
+            if tweet.data["public_metrics"]["like_count"] < delete_like_threshold and not int(tweet.data["id"]) in bookmarks: 
+                client.delete_tweet(tweet.data["id"], user_auth=False)
                 deleted_tweets += 1
 
                 print(tweet.data["id"])
@@ -71,41 +66,47 @@ if __name__ == "__main__":
     delete_like_threshold = int(config["defaults"].get("delete_like_threshold", 50))
     delete_days_threshold = int(config["defaults"].get("delete_days_threshold", 7))
 
-    bearer_token = config["auth"].get("bearer_token", None)
-    consumer_key = config["auth"].get("consumer_key", None)
+    client_id = config["auth"].get("client_id", None)
+    client_secret = config["auth"].get("client_secret", None)
     consumer_secret = config["auth"].get("consumer_secret", None)
     access_token = config["auth"].get("access_token", None)
-    access_token_secret = config["auth"].get("access_token_secret", None)
 
-    if bearer_token is None:
-        bearer_token = config["auth"]["bearer_token"] = input("bearer_token: ")
-    if consumer_key is None:
-        consumer_key = config["auth"]["consumer_key"] = input("consumer_key: ")
+    if client_id is None:
+        client_id = config["auth"]["client_id"] = input("client_id: ")
+    if client_secret is None:
+        client_secret = config["auth"]["client_secret"] = input("client_secret: ")
     if consumer_secret is None:
         consumer_secret = config["auth"]["consumer_secret"] = input("consumer_secret: ")
 
     with open("config.ini", "w") as f:
         config.write(f)
 
-    if access_token is None or access_token_secret is None:
-        app = Flask(__name__)
+    if access_token is None:
+        redirect_uri = f"http://{flask_host}:{flask_port}"
 
+        oauth2_user_handler = tweepy.OAuth2UserHandler(
+            client_id=client_id,
+            redirect_uri=redirect_uri,
+            scope=["tweet.read", "tweet.write", "users.read", "bookmark.read"],
+            client_secret=client_secret
+        )
+
+        auth_url = oauth2_user_handler.get_authorization_url()
+
+        app = Flask(__name__)
         webserver = threading.Thread(target=lambda: app.run(host=flask_host, port=flask_port, debug=False, use_reloader=False)).start()
 
         @app.route("/", methods=["GET"])
         def get_auth():
-            args = request.args
-            auth_data = { "oauth_token": args.get("oauth_token"), "oauth_verifier": args.get("oauth_verifier") }
-            threading.Thread(target=lambda: auth_handler(auth_data)).start()
+            response_uri = f"{redirect_uri}/?{request.query_string.decode()}"
+            access_token = oauth2_user_handler.fetch_token(response_uri)["access_token"]
+
+            threading.Thread(target=lambda: auth_handler(access_token)).start()
 
             return redirect("https://twitter.com")
 
-        callback_url = f"http://{flask_host}:{flask_port}"
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
-        auth_url = auth.get_authorization_url()
 
         print(f"Opening {auth_url}...")
         webbrowser.open(auth_url, new=0, autoraise=True)
     else:
-        auth_handler()
+        auth_handler(access_token)
